a7cfff26f61a38521c2e098328d3aa84dc3532d6
zeeshan <zeeshanali8512@gmail.com>
2025-06-10 15:05:08 +0530

    testing if_else

---
+++ if_else_statement/main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main(){
+a :=20
+b :=30
+if b<a{
+	fmt.Println("B is greater")
+}else{
+	fmt.Println("A is greater")
+}
+
+}
+
--- multiple_defer/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	defer fmt.Println("hell mr zeeshan")
-	defer fmt.Println("hello mr sibu")
-	defer fmt.Println("hello mr mobin")
-}
