9aa8209709d173f97c93db43ebfb2ca67038a08c
zeeshan <zeeshanali8512@gmail.com>
2025-06-17 08:28:56 +0530

    check formated time

---
+++ Time-apne_format_me_krna/main.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+import "time"
+func main(){
+	currentTime:=time.Now()
+	fmt.Println("Current time",currentTime)
+
+	formatted:=time.Now().Format("2006-01-05,Monday,15:01:02")
+	fmt.Println("Formatted Time",formatted)
+}
